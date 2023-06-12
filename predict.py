617dda64efe6e2bdd73c77beff6715a215f69581
Vadim Storozhev <ssvadim@posteo.net>
2023-06-13 03:01:28 +0500

    change model to whisper-medium-uz

--- predict.py
+++ predict.py
@@ -2,7 +2,6 @@
 download model weights to /data
 wget -O - https://pyannote-speaker-diarization.s3.eu-west-2.amazonaws.com/data-2023-03-25-02.tar.gz | tar xz -C /
 wget -P /data/whisper https://openaipublic.azureedge.net/main/whisper/models/d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f/medium.en.pt
-# wget -P /data/whisper https://openaipublic.azureedge.net/main/whisper/models/345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1/medium.pt
 """
 
 import json
@@ -47,7 +46,7 @@ class Predictor(BasePredictor):
         })
         self.diarization_post = DiarizationPostProcessor()
 
-        with open(f"/data/whisper/medium.en.pt", "rb") as f:
+        with open(f"/data/whisper/pytorch_model.bin", "rb") as f:
             checkpoint = torch.load(f, map_location="cpu")
             dims = ModelDimensions(**checkpoint["dims"])
         self.whisper = Whisper(dims)
